38a5719ab1f24e3f064f7ceb164ae06a4be7ffa4
nhmnhat1997 <nhmnhat1997@gmail.com>
2019-05-12 18:02:00 +0700

    Hiển thị chi tiết thông tin Địa điểm, nhà hàng, khách sạn

--- app/src/main/java/com/assignment/travelassistant/main_activity/place/PlaceDetail.java
+++ app/src/main/java/com/assignment/travelassistant/main_activity/place/PlaceDetail.java
@@ -1,16 +1,114 @@
 package com.assignment.travelassistant.main_activity.place;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.assignment.travelassistant.R;
+import com.assignment.travelassistant.model.Place;
+import com.bumptech.glide.Glide;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class PlaceDetail extends AppCompatActivity {
+    private boolean permisionGranted = false;
+    private GoogleMap gMap;
+    private Intent intent;
+    TextView tvName, tvAddress, tvTime, tvContent;
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_detail);
+        intent = getIntent();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle(intent.getStringExtra("name"));
+
+        tvName = (TextView) findViewById(R.id.tv_name);
+        tvAddress = (TextView) findViewById(R.id.tv_address);
+        tvTime = (TextView) findViewById(R.id.tv_time);
+        tvContent = (TextView) findViewById(R.id.tv_content);
+        img = (ImageView) findViewById(R.id.img);
+
+        tvName.setText(intent.getStringExtra("name"));
+        tvAddress.setText("Address: " + intent.getStringExtra("address"));
+        tvTime.setText("Open hours: " + intent.getStringExtra("open") + " - " + intent.getStringExtra("close"));
+        tvContent.setText(intent.getStringExtra("detail"));
+        Glide.with(this).load(intent.getStringExtra("img")).into(img);
+
+        getPermision();
+    }
+
+    public void getPermision() {
+        String[] permission = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        if (Build.VERSION.SDK_INT >=Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(PlaceDetail.this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    || ContextCompat.checkSelfPermission(PlaceDetail.this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    || ContextCompat.checkSelfPermission(PlaceDetail.this,Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(PlaceDetail.this,permission,1);
+            } else {
+                initMap();
+            }
+        } else  {
+            initMap();
+        }
+    }
+
+    public void initMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                gMap = googleMap;;
+                if (ActivityCompat.checkSelfPermission(PlaceDetail.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(PlaceDetail.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                gMap.setMyLocationEnabled(true);
+                moveCamera(new LatLng(intent.getDoubleExtra("longittude",0),intent.getDoubleExtra("latitude",0)),15f,intent.getStringExtra("name"));
+
+            }
+        });
+    }
+
+    public void moveCamera(LatLng latLng, float zoom, String title) {
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title(title);
+        gMap.addMarker(markerOptions);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permisionGranted = false;
+        if (requestCode == 1) {
+            permisionGranted = true;
+            initMap();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
